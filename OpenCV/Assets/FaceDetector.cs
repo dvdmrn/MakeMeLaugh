1a99c1a429a2e9d24c45c788399ab5f1d66de6a5
sandybannisse <sandybannisse@gmail.com>
2024-01-27 12:06:10 -0500

    OpenCV - FaceLocation

--- OpenCV/Assets/FaceDetector.cs
+++ OpenCV/Assets/FaceDetector.cs
@@ -7,7 +7,7 @@ public class FaceDetector : MonoBehaviour
 {
     // Start is called before the first frame update
     WebCamTexture _webCamTexture;
-
+    CascadeClassifier cascade;
 
 
 
@@ -17,12 +17,21 @@ public class FaceDetector : MonoBehaviour
 
         _webCamTexture = new WebCamTexture(devices[0].name);
         _webCamTexture.Play();
-        
+        cascade = new CascadeClassifier(Application.dataPath + @"/haarcascade_frontalface_default.xml");
     }
 
     // Update is called once per frame
     void Update()
     {
         GetComponent<Renderer>().material.mainTexture = _webCamTexture;
+        Mat frame = OpenCvSharp.Unity.TextureToMat(_webCamTexture);
+        findNewFace(frame);
+    }
+
+    void findNewFace(Mat frame) {
+        var faces = cascade.DetectMultiScale(frame, 1.1, 2, HaarDetectionType.ScaleImage);
+        if(faces.Length >= 1){
+            Debug.Log(faces[0].Location);
+        }
     }
 }
