cca306ceecde87a60b60b9b8d42272aa34c1d38d
sandybannisse <sandybannisse@gmail.com>
2024-01-27 12:53:13 -0500

    OpenCV - Add Rectangle

--- Assets/FaceDetector.cs
+++ Assets/FaceDetector.cs
@@ -8,7 +8,7 @@ public class FaceDetector : MonoBehaviour
     // Start is called before the first frame update
     WebCamTexture _webCamTexture;
     CascadeClassifier cascade;
-
+    OpenCvSharp.Rect MyFace;
 
 
     void Start()
@@ -26,12 +26,24 @@ public class FaceDetector : MonoBehaviour
         GetComponent<Renderer>().material.mainTexture = _webCamTexture;
         Mat frame = OpenCvSharp.Unity.TextureToMat(_webCamTexture);
         findNewFace(frame);
+        display(frame);
     }
 
     void findNewFace(Mat frame) {
         var faces = cascade.DetectMultiScale(frame, 1.1, 2, HaarDetectionType.ScaleImage);
         if(faces.Length >= 1){
             Debug.Log(faces[0].Location);
+            MyFace = faces[0];
         }
+    }
+
+    void display(Mat frame) {
+        if (MyFace != null) {
+            frame.Rectangle(MyFace, new Scalar(250,0,0),2);
+
+        }
+        Texture newtexture = OpenCvSharp.Unity.MatToTexture(frame);
+        GetComponent<Renderer>().material.mainTexture = newtexture;
+
     }
 }
